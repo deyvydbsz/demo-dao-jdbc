6081ad98a39e7a5eca7857e3b810779b113629cd
Deyvyd Zaccariotto <deyvydbsz@live.jp>
2022-01-27 16:39:15 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,6 +40,12 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New seller id = " + newSeller.getId());
 		
+		System.out.println("\n===== TEST 5: seller Update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
+		
 
 	}
 }
